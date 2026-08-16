b3c3e7a833d542a4324b863109a7023323d7fe4a
Anna.K <flysnow@ukr.net>
2026-08-16 19:27:38 +0300

    Node_js BASICS

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// src/index.js
+// ------------------------------------------------------
+// npm init -y
+// npm install -D nodemon
+// npm init @eslint/config@latest
+
+// ================================
+// -- TEST 1 - message
+// ================================
+// const message = 'Hello world';
+// console.log(message);
+
+// ================================
+// -- TEST 2 - jsonplaceholder
+// ================================
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
